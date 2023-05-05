a0e522f28ac66bcf81d5953d6f0c49df98615367
shreya20473 <shreya20473@iiitd.ac.in>
2023-05-06 02:24:19 +0530

    razorPay added
    integrated razorpay

--- Project_Impact2/app/src/main/java/com/example/project_impact/Navigation_Fragments/donate_fragment.java
+++ Project_Impact2/app/src/main/java/com/example/project_impact/Navigation_Fragments/donate_fragment.java
@@ -1,5 +1,6 @@
 package com.example.project_impact.Navigation_Fragments;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,60 +8,69 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.project_impact.R;
+import com.example.project_impact.User_Landing_Page;
+import com.razorpay.Checkout;
+import com.razorpay.PaymentResultListener;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link donate_fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class donate_fragment extends Fragment {
+import org.json.JSONException;
+import org.json.JSONObject;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public donate_fragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment donate_fragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static donate_fragment newInstance(String param1, String param2) {
-        donate_fragment fragment = new donate_fragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+public class donate_fragment extends Fragment implements PaymentResultListener {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_donate_fragment, container, false);
+        View view= inflater.inflate(R.layout.fragment_donate_fragment, container, false);
+
+        Checkout.preload(getContext());
+
+        Button btnPayNow = view.findViewById(R.id.donateButton);
+        btnPayNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startPayment();
+            }
+        });
+
+        return view;
     }
+    public void startPayment() {
+        Checkout checkout = new Checkout();
+        checkout.setKeyID("rzp_test_tgE3Us5EWfozOi");
+
+        User_Landing_Page donateFragment;
+
+        //checkout.setImage(R.drawable.logo);
+        try {
+            JSONObject options = new JSONObject();
+            options.put("name", "ABC Foundation");
+            options.put("description", "Test payment");
+            options.put("currency", "INR");
+            options.put("amount", "1000");
+           //  checkout.open(donateFragment, options);
+        }
+
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public void onPaymentSuccess(String s) {
+        Toast.makeText(getContext(), "Payment successful", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPaymentError(int i, String s) {
+        Toast.makeText(getContext(), "Payment error: " + s, Toast.LENGTH_SHORT).show();
+    }
+
 }
