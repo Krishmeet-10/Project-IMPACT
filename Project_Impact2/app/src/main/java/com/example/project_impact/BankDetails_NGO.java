368e2e289e44e9bdb850377d7c9a311e6f26d907
shreya20473 <shreya20473@iiitd.ac.in>
2023-05-09 05:28:34 +0530

    cscs

--- Project_Impact2/app/src/main/java/com/example/project_impact/BankDetails_NGO.java
+++ Project_Impact2/app/src/main/java/com/example/project_impact/BankDetails_NGO.java
@@ -1,10 +1,17 @@
 package com.example.project_impact;
 
+import static java.security.AccessController.getContext;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.project_impact.Navigation_Fragments.Razorpay_passthrough;
+import com.example.project_impact.Ngo_landing_fragments.ngo_Profile_fragment;
 
 public class BankDetails_NGO extends AppCompatActivity {
 
@@ -14,5 +21,15 @@ public class BankDetails_NGO extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bank_details_ngo);
+        bankDetailsButton = findViewById(R.id.btn_org_bank_details_submit);
+
+        bankDetailsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(BankDetails_NGO.this, ngo_Profile_fragment.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
