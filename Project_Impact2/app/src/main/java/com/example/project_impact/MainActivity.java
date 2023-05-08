7522147e3fa9d9e6cdff286d16cc8db544faba93
Krishmeet-10 <88630118+Krishmeet-10@users.noreply.github.com>
2023-05-09 04:58:40 +0530

    merge_fixed

--- Project_Impact2/app/src/main/java/com/example/project_impact/MainActivity.java
+++ Project_Impact2/app/src/main/java/com/example/project_impact/MainActivity.java
@@ -15,7 +15,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        loadUser();
         setContentView(R.layout.activity_main);
           mFirebaseAuth= FirebaseAuthManager.getInstance();
 
